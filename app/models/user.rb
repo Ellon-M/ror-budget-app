0d2dd704cf421241fd396290e2d051bdc38448e3
Ellon M <mordecaimwema11@gmail.com>
2023-04-25 14:10:02 +0300

    add associations to models, create migrations for tables

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :category
+  has_many :payments, through: :category, dependent: :destroy
+
+  validates :name, presence: true
+end
